efdbc1fb850e4022a4acdad10a05439a208a379e
emilyj4482 <emilyj4482@gmail.com>
2024-10-27 22:40:12 +0900

    first commit

---
+++ wotd_RxSwift/SceneDelegate.swift
@@ -0,0 +1,45 @@
+//
+//  SceneDelegate.swift
+//  wotd_RxSwift
+//
+//  Created by EMILY on 27/10/2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = TabBarViewController()
+        window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+        
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+        
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        
+    }
+
+
+}
+
---
+++ wotd_RxSwift/TabBarViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  wotd_RxSwift
+//
+//  Created by EMILY on 27/10/2024.
+//
+
+import UIKit
+
+class TabBarViewController: UIViewController {
+    
+    private lazy var testLabel: UILabel = {
+        let label = UILabel()
+        
+        label.textColor = .black
+        label.font = .preferredFont(forTextStyle: .extraLargeTitle)
+        
+        return label
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addSubview()
+        layout()
+        bind()
+    }
+    
+    private func addSubview() {
+        view.addSubview(testLabel)
+        testLabel.translatesAutoresizingMaskIntoConstraints = false
+    }
+
+    private func layout() {
+        
+        let superView = view.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            testLabel.centerXAnchor.constraint(equalTo: superView.centerXAnchor),
+            testLabel.centerYAnchor.constraint(equalTo: superView.centerYAnchor)
+        ])
+    }
+    
+    private func bind() {
+        testLabel.text = "Hello"
+        
+    }
+}
+
+#Preview {
+    TabBarViewController()
+}
